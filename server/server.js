a43b0388dcb9890a178a1d33715986e04135693f
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-03-28 14:32:10 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get("/server-alive", (req, res) => {
   res.send("Abhi hum zinda hai");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
